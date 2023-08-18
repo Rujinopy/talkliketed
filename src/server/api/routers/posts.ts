e4f21c56510fae2b3a4b57c5e6d3f62a4db71915
Rujinopy <ruth3442@gmail.com>
2023-08-18 19:21:15 +0700

    delete unused api

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,36 +1,8 @@
 import {z} from 'zod';
 
 import {createTRPCRouter, publicProcedure} from '~/server/api/trpc';
-import { clerkClient } from '@clerk/nextjs/server';
-
 
 export const postsRouter = createTRPCRouter({
-    hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-    getAll: publicProcedure.query(async ({ ctx }) => {
-        const users = await clerkClient.users.getUserList()
-        console.log("server")
-        console.log(users)
-        return ctx.prisma.posts.findMany();
-    }
-    )
-    ,
-    //delete posts that was checked input = set of id
-    delete: publicProcedure
-    .input(z.string())
-    .mutation(async ({ input, ctx }) => {
-        return await ctx.prisma.posts.delete({
-            where: {
-                id: input
-            }
-        });
-    }),
-
     stripeRetrieve: publicProcedure
     .input(z.object({ payment_intent_id: z.string() }))
     .query(async ({ input, ctx }) => {
