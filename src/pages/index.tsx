f8db0f8333a11de47ee8dbe4efc516044f8cdc62
Rujinopy <ruth3442@gmail.com>
2023-08-18 20:58:47 +0700

    edit landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,9 +41,9 @@ const Home: NextPage = (props) => {
   return (
     <>
       <NavbarWithoutCam style="sticky md:flex top-0 z-50" />
-      <div className="container w-screen flex min-h-screen flex-col items-center justify-center border-b-2 border-black bg-[#ffb2ef]">
+      <div className="w-screen flex min-h-screen flex-col items-center justify-center border-b-2 border-black bg-[#ffb2ef]">
         <main
-          className="flex h-screen w-screen flex-col items-center justify-center border-black 
+          className="flex h-screen w-full max-w-7xl flex-col items-center justify-center border-black 
         bg-yellow-200 text-center font-mono md:w-[70vw] md:border-x-2 md:pb-10"
         >
           <div className="flex h-full w-full flex-col py-10 md:flex-row md:pb-5">
